5176bee619d0b74d36c3456f217a2b6da244d58e
Masoud Ghorbani <masoudghorbani@pm.me>
2024-01-29 21:31:57 +0100

    ci: update CI configuration and environment setup

--- test/setupEnvs.ts
+++ test/setupEnvs.ts
@@ -5,18 +5,26 @@ export const setupEnvs = async (): Promise<void> => {
     ignoreCache: true,
   })
 
-  process.env.AWS_ACCESS_KEY_ID =
-    iniFiles.credentialsFile[
-      process.env.AWS_PROFILE ?? 'default'
-    ].aws_access_key_id
-  process.env.AWS_SECRET_ACCESS_KEY =
-    iniFiles.credentialsFile[
-      process.env.AWS_PROFILE ?? 'default'
-    ].aws_secret_access_key
-  process.env.AWS_SESSION_TOKEN =
-    iniFiles.credentialsFile[
-      process.env.AWS_PROFILE ?? 'default'
-    ].aws_session_token
-  process.env.AWS_REGION =
-    iniFiles.configFile[process.env.AWS_PROFILE ?? 'default'].region
+  const isCI = process.env.CI === 'true'
+  const awsENVsExist =
+    process.env.AWS_ACCESS_KEY_ID &&
+    process.env.AWS_SECRET_ACCESS_KEY &&
+    process.env.AWS_SESSION_TOKEN
+
+  if (!isCI || awsENVsExist) {
+    process.env.AWS_ACCESS_KEY_ID =
+      iniFiles.credentialsFile[
+        process.env.AWS_PROFILE ?? 'default'
+      ].aws_access_key_id
+    process.env.AWS_SECRET_ACCESS_KEY =
+      iniFiles.credentialsFile[
+        process.env.AWS_PROFILE ?? 'default'
+      ].aws_secret_access_key
+    process.env.AWS_SESSION_TOKEN =
+      iniFiles.credentialsFile[
+        process.env.AWS_PROFILE ?? 'default'
+      ].aws_session_token
+    process.env.AWS_REGION =
+      iniFiles.configFile[process.env.AWS_PROFILE ?? 'default'].region
+  }
 }
